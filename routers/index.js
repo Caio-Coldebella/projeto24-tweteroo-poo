3cd8544fdc4987bc880576ea55cf9cf1cedef3cc
ElderGr <gbielrs@gmail.com>
2023-03-01 23:09:49 -0300

    feat: review

--- routers/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import tweets from "./tweetsrouter.js";
-import { signupcontrolller } from "../controllers/signup-controller.js";
-
-const router = Router();
-router.use(tweets, signupcontrolller);
-router.post('/sign-up');
-export default router;
--- routers/tweetsrouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { gettwwets, getusertweets, posttweets } from "../controllers/tweets-controller.js";
-
-const tweets = Router();
-tweets.post('/tweets', posttweets);
-tweets.get('/tweets', gettwwets);
-tweets.get('/tweets/:username', getusertweets);
-export default tweets;
